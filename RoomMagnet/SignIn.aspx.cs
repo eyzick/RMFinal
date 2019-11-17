05ce547b5fa21040c6c4bc07c1013958e138d633
Isaac Gamble <gambleim@dukes.jmu.edu>
2019-11-17 16:20:46 -0500

    change

--- RoomMagnet/SignIn.aspx.cs
+++ RoomMagnet/SignIn.aspx.cs
@@ -16,7 +16,7 @@ public partial class SignIn : System.Web.UI.Page
         {
             if (Request.Cookies["UNAME"] != null && Request.Cookies["PWD"] != null)
             {
-                UserName.Text = Request.Cookies["UNAME"].Value;
+                tbEmail.Text = Request.Cookies["UNAME"].Value;
                 Password.Attributes["value"] = Request.Cookies["PWD"].Value;
                 CheckBox1.Checked = true;
             }
@@ -25,61 +25,71 @@ public partial class SignIn : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        String CS = ConfigurationManager.ConnectionStrings["RoomMagnet"].ConnectionString;
-        using (SqlConnection con = new SqlConnection(CS))
+        Boolean success = false;
+        int id = 0;
+        String usertype = "";
+
+        System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection();
+        sc.ConnectionString = @"Data Source=aay09edjn65sf6.cpcbbo8ggvx6.us-east-1.rds.amazonaws.com;Initial Catalog=RoomMagnet;Persist Security Info=True;User ID=fahrenheit;Password=cis484fall";
+        sc.Open();
+        System.Data.SqlClient.SqlCommand match = new System.Data.SqlClient.SqlCommand();
+        match.Connection = sc;
+
+        match.CommandText = "select passwordhash from[db_owner].[AdminPassword] where Email = @Email " +
+            "union select passwordhash from[dbo].[HostPassword] where Email = @Email " +
+            "union select passwordhash from[dbo].[TenantPassword] where Email = @Email";
+
+        match.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Email", tbEmail.Text));
+        System.Data.SqlClient.SqlDataReader reader = match.ExecuteReader(); // create a reader
+
+        if (reader.HasRows)
         {
-            SqlCommand cmd = new SqlCommand("select * from [dbo].[RMUser] where Email='" + UserName.Text + "' or LastName='" + Password.Text + "'", con);
-            con.Open();
-            SqlDataAdapter sda = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
-
-            if (dt.Rows.Count != 0)
+            while (reader.Read()) // this will read the single record that matches the entered usename
             {
-                Session["USERID"] = dt.Rows[0]["UserID"].ToString();
-                Session["USEREMAIL"] = dt.Rows[0]["Email"].ToString();
-
-                if (CheckBox1.Checked)
+                string storedHash = reader["PasswordHash"].ToString(); // store the database password into this varable
+                if (PasswordHash.ValidatePassword(Password.Text, storedHash)) // if the entered password matches what is stored, it will show success
                 {
-                    Response.Cookies["UNAME"].Value = UserName.Text;
-                    Response.Cookies["PWD"].Value = Password.Text;
-
-                    Response.Cookies["UNAME"].Expires = DateTime.Now.AddDays(15);
-                    Response.Cookies["PWD"].Expires = DateTime.Now.AddDays(15);
+                    Label1.Text = "Success!";
+                    success = true;
                 }
                 else
-                {
-                    Response.Cookies["UNAME"].Expires = DateTime.Now.AddDays(-1);
-                    Response.Cookies["PWD"].Expires = DateTime.Now.AddDays(-1);
-                }
-                string Utype;
-                Utype = dt.Rows[0]["UserType"].ToString().Trim();
-
-                Session["USERTYPE"] = Utype.ToString();
-
-                if (Utype == "t")
-                {
-                    Session["USERNAME"] = UserName.Text;
-                    Response.Redirect("~/TenantDashboard.aspx");
-                    
-
-                }
-                if (Utype == "a")
-                {
-                    Session["USERNAME"] = UserName.Text;
-                    Response.Redirect("~/AdminDashBoard.aspx");
-                }
-                if (Utype == "h")
-                {
-                    Session["USERNAME"] = UserName.Text;
-                    Response.Redirect("~/HostDashBoard.aspx");
-                }
-
-
+                    Label1.Text = "Password is wrong.";
             }
-            else
+        }
+        else // if the username does not exist, it will show failure.
+            Label1.Text = "Login failed";
+        sc.Close();
+        if (success == true)
+        {
+            sc.Open();
+            System.Data.SqlClient.SqlCommand matchID = new System.Data.SqlClient.SqlCommand();
+            matchID.Connection = sc;
+            //matchID.CommandText = "Select AdminID from [db_owner].[AdminPassword] where Email = @Email";
+            matchID.CommandText = "select adminid from[db_owner].[AdminPassword] where Email = @Email " +
+                "union select hostid from[dbo].[HostPassword] where Email = @Email " +
+                "union select tenantid from[dbo].[TenantPassword] where Email = @Email";
+            matchID.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Email", tbEmail.Text));
+            id = (Int32)matchID.ExecuteScalar();
+            Session["globalID"] = id;
+            Label1.Text = "Success! ID is: " + id;
+
+            System.Data.SqlClient.SqlCommand type = new System.Data.SqlClient.SqlCommand();
+            type.Connection = sc;
+
+            type.CommandText = "select UserType from [dbo].[RMUser] where userid = " + id;
+            usertype = type.ExecuteScalar().ToString();
+
+            switch (usertype)
             {
-                lblError.Text = "Invalid Username or Password !";
+                case "t":
+                    Response.Redirect("TenantDashboard.aspx");
+                    break;
+                case "h":
+                    Response.Redirect("HostDashBoard.aspx");
+                    break;
+                case "a":
+                    Response.Redirect("AdminDashboard.aspx");
+                    break;
             }
         }
     }
