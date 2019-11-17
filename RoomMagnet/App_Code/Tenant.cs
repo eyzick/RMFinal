51dabe1ae899c35a4d1c7c63fb25dca6a7a053ea
Isaac Gamble <gambleim@dukes.jmu.edu>
2019-11-17 15:18:05 -0500

    log in

---
+++ RoomMagnet/App_Code/Tenant.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tenant
+/// </summary>
+public class Tenant
+{
+    private int TenantID;
+    private String FirstName;
+    private String LastName;
+    private String EmailAddress;
+    private String PhoneNumber;
+    private String HouseNumber;
+    private String Street;
+    private String CityCounty;
+    private String HomeState;
+    private String Zip;
+    private DateTime DateOfBirth;
+    private string userType;
+    
+
+    public Tenant()
+    {
+
+    }
+    public Tenant(String FirstName, String LastName, String EmailAddress, String PhoneNumber, String HouseNumber, String Street, String CityCounty, String HomeState, String Zip, DateTime DateOfBirth, String userType)
+    {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.EmailAddress = EmailAddress;
+        this.PhoneNumber = PhoneNumber;
+        this.HouseNumber = HouseNumber;
+        this.Street = Street;
+        this.CityCounty = CityCounty;
+        this.HomeState = HomeState;
+        this.Zip = Zip;
+        this.DateOfBirth = DateOfBirth;
+        this.userType = userType;
+
+
+
+    }
+    public void setPhoneNumber(string phoneNumber)
+    {
+        this.PhoneNumber = phoneNumber;
+    }
+    public string getPhoneNumber()
+    {
+        return PhoneNumber;
+    }
+
+    public string getUserType()
+    {
+
+        return this.userType;
+    }
+
+    public int GetTenantID()
+    {
+        return TenantID;
+    }
+    public void setTenantID(int TenantID)
+    {
+        this.TenantID = TenantID;
+
+    }
+
+    public String GetFirstName()
+    {
+        return FirstName;
+    }
+    public void SetFirstName(String FirstName)
+    {
+        this.FirstName = FirstName;
+    }
+
+    public String GetLastName()
+    {
+        return LastName;
+    }
+    public void SetLastName(String LastName)
+    {
+        this.LastName = LastName;
+    }
+
+    public String GetEmailAddress()
+    {
+        return EmailAddress;
+    }
+    public void SetEmailAddress(String EmailAddress)
+    {
+        this.EmailAddress = EmailAddress;
+    }
+
+    public String GetHouseNumber()
+    {
+        return HouseNumber;
+    }
+    public void SetHouseNumber(String HouseNumber)
+    {
+        this.HouseNumber = HouseNumber;
+
+    }
+    public String GetStreet()
+    {
+        return Street;
+    }
+    public void SetStreet(String Street)
+    {
+        this.Street = Street;
+
+    }
+    public String GetCityCounty()
+    {
+        return CityCounty;
+    }
+    public void SetCityCounty(String CityCounty)
+    {
+        this.CityCounty = CityCounty;
+
+    }
+
+    public String GetHomeState()
+    {
+        return HomeState;
+    }
+    public void SetHomeState(String HomeState)
+    {
+        this.HomeState = HomeState;
+
+    }
+    public String GetZip()
+    {
+        return Zip;
+    }
+    public void SetZip(String Zip)
+    {
+        this.Zip = Zip;
+
+    }
+    public DateTime GetDateOfBirth()
+    {
+        return DateOfBirth;
+    }
+    public void SetDateOfBirth(DateTime DateOfBirth)
+    {
+        this.DateOfBirth = DateOfBirth;
+
+    }
+
+    
+    
+
+
+}
--- RoomMagnet/SignIn.aspx.cs
+++ RoomMagnet/SignIn.aspx.cs
@@ -28,55 +28,76 @@ public partial class SignIn : System.Web.UI.Page
         String CS = ConfigurationManager.ConnectionStrings["RoomMagnet"].ConnectionString;
         using (SqlConnection con = new SqlConnection(CS))
         {
-            SqlCommand cmd = new SqlCommand("select * from [dbo].[RMUser] where Email='" + UserName.Text + "' and Password='" + Password.Text + "'", con);
-            con.Open();
-            SqlDataAdapter sda = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
+            Boolean success = false;
+            int id = 0;
+            String usertype = "";
 
-            if (dt.Rows.Count != 0)
+            System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection();
+            sc.ConnectionString = @"Data Source=aay09edjn65sf6.cpcbbo8ggvx6.us-east-1.rds.amazonaws.com;Initial Catalog=RoomMagnet;Persist Security Info=True;User ID=fahrenheit;Password=cis484fall";
+            sc.Open();
+            System.Data.SqlClient.SqlCommand match = new System.Data.SqlClient.SqlCommand();
+            match.Connection = sc;
+
+            match.CommandText = "select passwordhash from[db_owner].[AdminPassword] where Email = @Email " +
+                "union select passwordhash from[dbo].[HostPassword] where Email = @Email " +
+                "union select passwordhash from[dbo].[TenantPassword] where Email = @Email";
+
+            match.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Email", UserName.Text));
+            System.Data.SqlClient.SqlDataReader reader = match.ExecuteReader(); // create a reader
+
+            if (reader.HasRows)
             {
-                Session["USERID"] = dt.Rows[0]["UserID"].ToString();
-                Session["USEREMAIL"] = dt.Rows[0]["Email"].ToString();
-
-                if (CheckBox1.Checked)
+                while (reader.Read()) // this will read the single record that matches the entered usename
                 {
-                    Response.Cookies["UNAME"].Value = UserName.Text;
-                    Response.Cookies["PWD"].Value = Password.Text;
-
-                    Response.Cookies["UNAME"].Expires = DateTime.Now.AddDays(15);
-                    Response.Cookies["PWD"].Expires = DateTime.Now.AddDays(15);
+                    string storedHash = reader["PasswordHash"].ToString(); // store the database password into this varable
+                    if (PasswordHash.ValidatePassword(Password.Text, storedHash)) // if the entered password matches what is stored, it will show success
+                    {
+                        Label1.Text = "Success!";
+                        success = true;
+                    }
+                    else
+                        Label1.Text = "Password is wrong.";
                 }
-                else
-                {
-                    Response.Cookies["UNAME"].Expires = DateTime.Now.AddDays(-1);
-                    Response.Cookies["PWD"].Expires = DateTime.Now.AddDays(-1);
-                }
-                string Utype;
-                Utype = dt.Rows[0]["UserType"].ToString().Trim();
-
-                Session["USERTYPE"] = Utype.ToString();
-
-                if (Utype == "T")
-                {
-                    Session["USERNAME"] = UserName.Text;
-                    Response.Redirect("~/TenantDashboard.aspx");
-                    
-
-                }
-                if (Utype == "A")
-                {
-                    Session["USERNAME"] = UserName.Text;
-                    Response.Redirect("~/AdminDashBoard.aspx");
-                }
-                if (Utype == "H")
-                {
-                    Session["USERNAME"] = UserName.Text;
-                    Response.Redirect("~/HostDashBoard.aspx");
-                }
-
-
             }
+            else // if the username does not exist, it will show failure.
+                Label1.Text = "Login failed";
+            sc.Close();
+            if (success == true)
+            {
+                sc.Open();
+                System.Data.SqlClient.SqlCommand matchID = new System.Data.SqlClient.SqlCommand();
+                matchID.Connection = sc;
+                //matchID.CommandText = "Select AdminID from [db_owner].[AdminPassword] where Email = @Email";
+                matchID.CommandText = "select adminid from[db_owner].[AdminPassword] where Email = @Email " +
+                    "union select hostid from[dbo].[HostPassword] where Email = @Email " +
+                    "union select tenantid from[dbo].[TenantPassword] where Email = @Email";
+                matchID.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Email", UserName.Text));
+                id = (Int32)matchID.ExecuteScalar();
+                Session["globalID"] = id;
+                Label1.Text = "Success! ID is: " + id;
+
+                System.Data.SqlClient.SqlCommand type = new System.Data.SqlClient.SqlCommand();
+                type.Connection = sc;
+
+                type.CommandText = "select UserType from [dbo].[RMUser] where userid = " + id;
+                usertype = type.ExecuteScalar().ToString();
+
+                switch (usertype)
+                {
+                    case "t":
+                        Response.Redirect("TenantDashboard.aspx");
+                        break;
+                    case "h":
+                        Response.Redirect("HostDashboard.aspx");
+                        break;
+                    case "a":
+                        Response.Redirect("AdminDashboard.aspx");
+                        break;
+                }
+            }
+
+
+        
             else
             {
                 lblError.Text = "Invalid Username or Password !";
