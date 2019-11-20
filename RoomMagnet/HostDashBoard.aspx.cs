05efe9794d98195d8d937a24d47d7d0f3daeed7d
Isaac Gamble <gambleim@dukes.jmu.edu>
2019-11-19 20:29:26 -0500

    cant force onto a site

--- RoomMagnet/HostDashBoard.aspx.cs
+++ RoomMagnet/HostDashBoard.aspx.cs
@@ -32,6 +32,10 @@ public partial class HostDashBoard : System.Web.UI.Page
     }
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["USERNAME"] == null)
+        {
+            Response.Redirect("Home.aspx");
+        }
 
         //Session["USERID"] = "226";
 
