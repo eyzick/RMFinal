55fe0cbbfb2d563d164ce7ff1e5af8f8482d6e28
labpatron <labpatron@BUR244-5173-COB>
2019-11-20 21:12:56 -0500

    index pt2

--- RoomMagnet/HostDashBoard.aspx.cs
+++ RoomMagnet/HostDashBoard.aspx.cs
@@ -34,7 +34,7 @@ public partial class HostDashBoard : System.Web.UI.Page
     {
         if (Session["USERNAME"] == null)
         {
-            Response.Redirect("Home.aspx");
+            Response.Redirect("index.aspx");
         }
 
         if (!IsPostBack)
